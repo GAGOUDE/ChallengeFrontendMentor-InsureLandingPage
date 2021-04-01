430e822d7364e59581c9be4cbb994df25ff6636b
Gaël-Freddy GAGOUDE <gael.gagoude@gmail.com>
2021-04-01 22:32:03 +0200

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,7 @@
+
+function toggleMenu() {
+        var menuToggle = document.querySelector('.header_toggle');
+        var links = document.querySelector('.header_links');
+        menuToggle.classList.toggle('active');
+        links.classList.toggle('active');
+        }
